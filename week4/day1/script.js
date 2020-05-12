717e3eda49505fed214a75c585a4a355684ed8ad
irinaignatenok <specterhauz@gmail.com>
2020-05-12 14:50:29 +0300

    dailyc_challenge

---
+++ week4/day1/script.js
@@ -0,0 +1,20 @@
+let first_line = "*********";
+let second_line = "* Hello *";
+let third_line = "* World *";
+let fourth_line = "* in    *";
+let fifth_line = "* a     *";
+let sixth_line = "* frame *";
+
+
+let pic = [first_line, second_line, third_line, fourth_line, fifth_line, sixth_line, first_line];
+people.forEach(function(string){
+    console.log(string);
+}
+)
+ *********
+ * Hello *
+ * World *
+ * in    *
+ * a     *
+ * frame *
+ *********
